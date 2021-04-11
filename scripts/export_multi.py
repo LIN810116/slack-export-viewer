bc1c5c8a33c1be9f93affa69c90d1efa34c7140d
Chinchien <clin864@aucklanduni.ac.nz>
2021-04-11 19:38:51 +1200

    Add a script to execute the export method

---
+++ scripts/export_multi.py
@@ -0,0 +1,10 @@
+from slackviewer.export import export_multi
+
+if __name__ == '__main__':
+    # archive = r"C:\\Users\\clin864\\breast_group\\slack_archive\\myWorkspace Slack export Feb 12 2021 - Apr 8 2021.zip"
+    # save_dir = r"C:\\Users\\clin864\\breast_group\\slack_archive"
+    # cache_dir = r"C:\\Users\\clin864\\breast_group\\slack_archive\\temp"
+    # export_multi(archive, save_dir=save_dir, cache_dir=cache_dir)
+
+    archive = r"C:\\Users\\clin864\\breast_group\\slack_archive\\myWorkspace Slack export Feb 12 2021 - Apr 8 2021.zip"
+    export_multi(archive)
